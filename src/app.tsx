6702b1de5da02505707e06d50edda2e398cc5ea4
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2018-10-18 22:43:06 +0200

    Implement basic components

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
+import { BodyComponent } from './pages/body';
+import { MenuComponent } from './pages/menu';
+import { css } from 'emotion';
+
+// Pending move styles to a file
 
 let App: React.StatelessComponent = () => (
-  <h1>It works!</h1>
+  <div className={
+    css`display: flex;
+   flex-direction: row`}>
+    <MenuComponent />
+    <BodyComponent />
+  </div>
 );
 
 App = hot(module)(App);
---
+++ src/pages/body/bodyComponent.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const BodyComponent: React.StatelessComponent = () => (
+	<h1>Hello Boby Component</h1>
+);
