b542b4baa9b69041b5f79e8be0b3d9699cf7273f
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2018-11-22 21:52:45 +0100

    Fix PR

--- src/components/sideBar/sideBarComponent.tsx
+++ src/components/sideBar/sideBarComponent.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 
 // TODO: Pending to rename to a best name
 export const SideBarComponent: React.StatelessComponent = () => (
-	<h1>Hello SideBar Component</h1>
+  <h1>Hello SideBar Component</h1>
 )
