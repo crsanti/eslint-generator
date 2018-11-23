8f6006fab3472c42237fbde9d936108a1b4a37fb
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2018-11-23 21:04:13 +0100

    Add semicolon and newline to the end of document

--- src/components/sideBar/sideBarComponent.tsx
+++ src/components/sideBar/sideBarComponent.tsx
@@ -3,4 +3,4 @@ import * as React from 'react';
 // TODO: Pending to rename to a best name
 export const SideBarComponent: React.StatelessComponent = () => (
   <h1>Hello SideBar Component</h1>
-)
+);
