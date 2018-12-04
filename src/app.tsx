c22ba42f5414fa2625ac3df9e3b6f2f943d1fa46
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2018-12-04 22:39:23 +0100

    Create resetStyles and itemStyles.

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,10 @@ import { hot } from 'react-hot-loader';
 import { MainComponent } from './components/main';
 import { SideBarComponent } from './components/sideBar';
 import * as styles from './appStyles';
+import { resetSyles } from './resetSyles';
 
 let App: React.StatelessComponent = () => (
-  <div className={styles.container}>
+  <div className={`${styles.container} ${resetSyles}`}>
     <SideBarComponent />
     <MainComponent />
   </div>
