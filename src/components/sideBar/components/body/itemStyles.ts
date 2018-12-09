0595122c05278b0efeb9dead69e91592aed2fa23
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2018-12-09 21:01:10 +0100

    rename variable from itemStyles

--- src/components/sideBar/components/body/itemStyles.ts
+++ src/components/sideBar/components/body/itemStyles.ts
@@ -2,7 +2,7 @@ import { css } from 'emotion';
 import blue from '@material-ui/core/colors/blue';
 
 // TODO: move to common colors file
-const colorItem = blue[300];
+const ruleItemColor = blue[300];
 
 export const item = css`
   list-style: none;
@@ -10,7 +10,7 @@ export const item = css`
   font-family: none;
   line-height: 2.5;
   & :hover {
-    color: ${colorItem};
+    color: ${ruleItemColor};
     cursor: pointer;
   }
 `;
