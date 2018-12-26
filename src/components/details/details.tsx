b4f3b61e567b85ae372c8ae3bbd333d7184d864c
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2018-12-26 18:44:07 +0100

    Remove types and description component from main and add to details components

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
+import { Description } from '../description';
 import { Title } from '../title';
+import { Types } from '../types';
 
 export const Details: React.StatelessComponent = () => (
-  <Title />
+  <>
+    <Title />
+    <Types />
+    <Description />
+  </>
 );
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
-import { Description } from '../description';
 import { Details } from '../details';
-import { Types } from '../types';
 import * as styles from './main.sytes';
 
 export const Main: React.StatelessComponent = () => (
   <div className={styles.main}>
     <Details />
-    <Types />
-    <Description />
   </div>
 );
