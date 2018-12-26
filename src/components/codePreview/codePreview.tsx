2f5719045627726126274d5875038b04fe3cde84
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2018-12-26 19:32:29 +0100

    Create codePreview component and add main component

---
+++ src/components/codePreview/codePreview.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const CodePreview: React.StatelessComponent = () => (
+  <div>
+    <p>Hello codePreview component</p>
+  </div>
+)
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { CodePreview } from '../codePreview';
 import { Details } from '../details';
 import * as styles from './main.sytes';
 
 export const Main: React.StatelessComponent = () => (
   <div className={styles.main}>
     <Details />
+    <CodePreview />
   </div>
 );
