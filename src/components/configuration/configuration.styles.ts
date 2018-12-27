bc1045e654dc2fe3ad4d7c5eb4b17f8647feeff9
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2018-12-27 23:09:09 +0100

    add styles to configuration component

--- src/components/configuration/configuration.styles.ts
+++ src/components/configuration/configuration.styles.ts
@@ -2,5 +2,21 @@ import { css } from 'emotion';
 
 export const configuration = css`
   float: left;
+  align-items: baseline;
   padding-left: 1.5rem;
+`;
+
+export const buttonsContainer = css`
+  display: flex;
+  margin-top: 1.2rem;
+`;
+
+export const severityButtons = css`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.2rem;
+`;
+
+export const textSpan = css`
+  margin-bottom: 1.2rem;
 `;
--- src/components/configuration/configuration.tsx
+++ src/components/configuration/configuration.tsx
@@ -1,13 +1,42 @@
+import Button from '@material-ui/core/Button';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
 import * as React from 'react';
 import * as styles from './configuration.styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 
 export const Configuration: React.StatelessComponent = () => (
   <div className={styles.configuration}>
-      <Tabs value={0} indicatorColor="primary">
-        <Tab label="Rule settings" />
-        <Tab label="Global settings" />
-      </Tabs>
+    <Tabs value={0} indicatorColor="primary">
+      <Tab label="Rule settings" />
+      <Tab label="Global settings" />
+    </Tabs>
+    <div className={styles.buttonsContainer}>
+      <Button variant="outlined">
+        unset
+      </Button>
+      <Button variant="outlined">
+        false
+      </Button>
+      <Button variant="outlined" color="primary">
+        true
+      </Button>
+    </div>
+    <div className={styles.severityButtons}>
+      <span className={styles.textSpan}>Severity</span>
+      <div>
+        <Button variant="outlined">
+          default
+        </Button>
+        <Button variant="outlined">
+          error
+        </Button>
+        <Button variant="outlined">
+          warning
+        </Button>
+        <Button variant="outlined">
+          none
+        </Button>
+      </div>
+    </div>
   </div>
 );
